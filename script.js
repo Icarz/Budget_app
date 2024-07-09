9a494bd4c42f7bd91dff894a8cec4a3146cf8f63
Mohammed Rerhaye <Mr.rghay@gmail.com>
2024-07-09 18:38:11 +0100

    almost done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /** defining the variables based on the query Selector (.class) */
 const errorMessage = document.querySelector(".error_message");
 const budgetInputEl = document.querySelector(".budget_input");
-const expenseDelEl = document.querySelector(".expenses_input");
+const expenseDeslEl = document.querySelector(".expenses_input");
 const expenseAmountEl = document.querySelector(".expenses_amount");
 const tblRecordEl = document.querySelector(".tbl_data");
 const cardsContainer = document.querySelector(".cards");
@@ -12,7 +12,7 @@ const cardsContainer = document.querySelector(".cards");
 
 const budgetCardEl = document.querySelector(".budget-card");
 const expensesCardEl = document.querySelector(".expenses-card");
-const balanceCardEl = document.querySelector(".balance-car");
+const balanceCardEl = document.querySelector(".balance-card");
 
 let itemList = [];
 let itemId = 0;
@@ -32,12 +32,39 @@ function btnEvents() {
   //* === budget Event ====*//
   btnExpensesCal.addEventListener("click", (e) => {
     e.preventDefault();
-    budgetFun();
+    expensesFun();
   });
 }
 
 /*** ==== calling the Events ====    */
 document.addEventListener("DOMContentLoaded", btnEvents);
+
+/*** ==== Expenses Function ==== */
+function expensesFun() {
+  let expensesDescValue = expenseDeslEl.value;
+  let expensesAmountValue = expenseAmountEl.value;
+  if (
+    expensesDescValue == "" ||
+    expensesAmountValue == "" ||
+    budgetInputEl < 0
+  ) {
+    errorsAlert("Please enter expenses description OR expense amount");
+  } else {
+    let amount = parseInt(expensesDescValue);
+    expenseAmountEl.value = "";
+    expenseDeslEl.value = "";
+
+    /* === Store the value inside of an Object === */
+
+    let expense = {
+      id: itemId,
+      title: expensesDescValue,
+      amount: expensesAmountValue,
+    };
+    itemId++;
+    itemList.push(expense);
+  }
+}
 
 /** === Budget function ===  */
 function budgetFun() {
@@ -55,10 +82,14 @@ function budgetFun() {
 function showBalance() {
   const expenses = totalExpenses();
   const total = parseInt(budgetCardEl.textContent) - expenses;
+  balanceCardEl.textContent = total;
 }
 
 /* ==== total Expenses ==== */
-function totalExpenses() {}
+function totalExpenses() {
+  let total = 30;
+  return total;
+}
 
 /** ===error message==== */
 function errorsAlert(message) {
